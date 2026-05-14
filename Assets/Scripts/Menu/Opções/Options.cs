0d8430007b954bf93d1c6b7abdcb1f7eb9d324cb
murilo <rm98215@estudante.fieb.edu.br>
2026-05-14 11:55:54 -0300

    menu com idiomas

---
+++ Assets/Scripts/Menu/Opções/Options.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button options;
+    public GameObject painelConfigs;
+    public bool optionsOpen;
+
+    void Start()
+    {
+        optionsOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (options.onClick != null && optionsOpen == false)
+        {
+            optionsOpen = true;
+            painelConfigs.SetActive(true);
+        }
+
+        if (options.onClick != null && optionsOpen == true)
+        {
+            optionsOpen = false;
+            painelConfigs.SetActive(false);
+        }*/
+    }
+
+    public void abrirPaineis(GameObject painel)
+    {
+        painel.SetActive(true);
+    }
+
+    public void fecharPaineis(GameObject painel)
+    {
+        painel.SetActive(false);
+    }
+}
